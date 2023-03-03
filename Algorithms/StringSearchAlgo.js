b1ef38bd3c408c3def1b85c5c83dbaf68dab6bcc
DiligentCoder-20022001 <siddharthschandran45@gmail.com>
2023-03-03 20:09:30 +0530

    String Search Algo optimized

--- Algorithms/StringSearchAlgo.js
+++ Algorithms/StringSearchAlgo.js
@@ -1,18 +1,50 @@
-// naive algorithm 
-function searchForString(pat, str){
+function getZArray(str, ZArray){
     var n = str.length; 
-    var m = pat.length; 
+    var l, r, k; 
+    l = 0, r = 0; 
+    ZArray[0] = 0; 
 
-    for(var i = 0; i <= n-m; i++){
-        var j = 0; 
-        for(j = 0; j < m; j++){
-            if(pat[j] != str[i+j])
-                break;
+    for(var i = 1; i < n; i++){
+        if(i > r){
+            l = i, r = i; 
+
+            while(r < n && str[r - l] === str[r]){
+                r++;
+            }
+            ZArray[i] = r - l; 
+            r--; 
         }
-        if(j == m)
-            return i; 
+        else{
+            var k = i - l; 
+            if(ZArray[k] < r - i + 1){
+                ZArray[i] = ZArray[k]; 
+            }
+            else{
+                l = i; 
+                while(r < n && str[r-l] == str[r])
+                    r++; 
+                ZArray[i] = r - l; 
+                r--;
+            }
+        }
+    }
+    return ZArray;
+}
+
+function findPattern(pat, str){
+    var concatString = pat + "$" + str; 
+
+    var n = concatString.length;
+    var ZArray = new Array(n); 
+    
+    ZArray = getZArray(concatString, ZArray); 
+    
+    for(var i = 0; i < n; i++){
+        if(ZArray[i] == pat.length){
+            return i - pat.length - 1; // as the string is concatenated
+        }
     }
     return -1; 
 }
 
-module.exports = {searchForString};
+module.exports = {findPattern: findPattern};
